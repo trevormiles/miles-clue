06bf489bd9fba520ad3a84544889dcd0a515bb2f
Trevor Miles <miles.trev5@gmail.com>
2023-12-03 10:38:34 -0700

    Configure database file to use .env

--- config/database.php
+++ config/database.php
@@ -55,10 +55,11 @@ return array(
 
                 'mysql' => array(
                         'driver'    => 'mysql',
-                        'host'      => 'localhost',
-                        'database'  => 'kjwekahxgr',
-                        'username'  => 'kjwekahxgr',
-                        'password'  => 'TXj8wxzrKZ',
+                        'host' => env('DB_HOST', '127.0.0.1'),
+                        'port' => env('DB_PORT', '3306'),
+                        'database' => env('DB_DATABASE', 'forge'),
+                        'username' => env('DB_USERNAME', 'forge'),
+                        'password' => env('DB_PASSWORD', ''),
                         'charset'   => 'utf8',
                         'collation' => 'utf8_unicode_ci',
                         'prefix'    => '',
